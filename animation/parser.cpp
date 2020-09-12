0bb105955cc7b8d139cb5f7ddcf051a81f12ddb2
Luc Bournaud <bournaud@et.esiea.fr>
2020-09-12 18:39:35 +0200

    Made the JSON parser !
    	{
    		"stops": [
    				{"red":1.0, "green":0.0, "blue":0.0,"white":0.0,"duration":1.0,"next_index":1},
    				{"red":0.0, "green":0.0, "blue":0.42,"white":0.0,"duration":1.0,"next_index":0}
    		],
    		"init_indexes": [
    				0,0,0,1,1,1
    		],
    		"init_steps": [
    				0.2,0.0
    		]
    	}

---
+++ animation/parser.cpp
@@ -0,0 +1,242 @@
+#include "parser.hpp"
+#include <cstring>
+#include <cmath>
+#include <limits>
+
+animation::parser::parser(void)
+{
+	yajl_parser = yajl_alloc(&callbacks,NULL,this);
+}
+animation::parser::~parser(void)
+{
+	yajl_free(yajl_parser);
+}
+std::string animation::parser::get_error(yajl_status status, const char *jsonText, size_t jsonTextLength)
+{
+	switch (status) {
+		case yajl_status_ok:
+			return std::string("Parsing success"); // <- If you see that there's a problem...
+		case yajl_status_error: {
+			unsigned char *error_str = yajl_get_error(yajl_parser,1,reinterpret_cast<const unsigned char*>(jsonText),jsonTextLength);
+			parse_error = std::string(reinterpret_cast<const char*>(error_str));
+			yajl_free_error(yajl_parser,error_str);
+		}
+		case yajl_status_client_canceled:
+			return parse_error;
+		default:
+			return std::string("Unknow status");
+	}
+}
+
+void animation::parser::reset_stop(void)
+{
+	memset(&current_stop,0,sizeof(current_stop));
+}
+
+int animation::parser::yajl_null(void *ctx)
+{
+	animation::parser *parser = static_cast<animation::parser*>(ctx);
+	if (parser->skip_depth) return 1;
+	parser->parse_error = "Unexpected null";
+	return 0;
+}
+int animation::parser::yajl_boolean(void *ctx, int boolVal)
+{
+	animation::parser *parser = static_cast<animation::parser*>(ctx);
+	if (parser->skip_depth) return 1;
+	parser->parse_error = "Unexpected boolean";
+	return 0;
+}
+
+int animation::parser::yajl_integer(void *ctx, long long integerVal)
+{
+	animation::parser *parser = static_cast<animation::parser*>(ctx);
+	if (parser->skip_depth) return 1;
+	// Check if the integer fit in a uint16_t
+	if (integerVal < 0) {
+		parser->parse_error = "Negative values are not supported";
+		return 0;
+	} else if (integerVal > std::numeric_limits<uint16_t>::max()) {
+		parser->parse_error = "s values are not supported";
+		return 0;
+	}
+	// Dispatch
+	switch (parser->current_key) {
+		case KEY_NONE:
+		case KEY_INIT_INDEXES:
+			switch (parser->tree_state) {
+				case TREE_INIT_INDEXES:
+					return parser->emit_initial_index(integerVal);
+				default:
+					parser->parse_error = "Unexpected integer";
+					return 0;
+			}
+		case KEY_NEXT_INDEX:
+			parser->current_stop.next_index = integerVal;
+			return 1;
+		default:
+			return animation::parser::yajl_double(ctx,integerVal);
+	}
+}
+int animation::parser::yajl_double(void *ctx, double doubleVal)
+{
+	animation::parser *parser = static_cast<animation::parser*>(ctx);
+	if (parser->skip_depth) return 1;
+	if (doubleVal < 0) {
+		parser->parse_error = "Negative values are not supported";
+		return 0;
+	}
+	switch (parser->current_key) {
+		case KEY_RED:
+			parser->current_stop.color.red = doubleVal;
+			return 1;
+		case KEY_GREEN:
+			parser->current_stop.color.green = doubleVal;
+			return 1;
+		case KEY_BLUE:
+			parser->current_stop.color.blue = doubleVal;
+			return 1;
+		case KEY_WHITE:
+			parser->current_stop.color.white = doubleVal;
+			return 1;
+		case KEY_DURATION:
+			parser->current_stop.duration = doubleVal*10;
+			return 1;
+		case KEY_INIT_STEPS:
+			if (doubleVal > 1) {
+				parser->parse_error = "\"init_steps\" values are in the [0..1] range (initial position ratio in the initial stop).";
+				return 0;
+			} else return parser->emit_initial_remaining(1-doubleVal);
+		case KEY_NEXT_INDEX:
+		case KEY_INIT_INDEXES: {
+			double i;
+			if (std::modf(doubleVal,&i)) {
+				parser->parse_error = "\"next_index\" and \"init_indexes\" only take integers";
+				return 0;
+			} else return yajl_integer(ctx,i);
+		}
+		default:
+			parser->parse_error = "Unexpected number";
+			return 0;
+	}
+}
+int animation::parser::yajl_string(void *ctx, const unsigned char * stringVal, size_t stringLen)
+{
+	animation::parser *parser = static_cast<animation::parser*>(ctx);
+	if (parser->skip_depth) return 1;
+	parser->parse_error = "Unexpected string";
+	return 0;
+}
+
+int animation::parser::yajl_map_key(void *ctx, const unsigned char *key_u, size_t stringLen)
+{
+	animation::parser *parser = static_cast<animation::parser*>(ctx);
+	if (parser->skip_depth) return 1;
+	// TODO Use a less naive way
+	const char* key = reinterpret_cast<const char*>(key_u);
+	if (parser->tree_state == TREE_ROOT_MAP) {
+		// We are in the root, expect a "stops", "init_indexes" or "init_step" subkey
+		if (!strncmp(key,"stops",stringLen))
+			parser->current_key = KEY_STOPS;
+		else if (!strncmp(key,"init_indexes",stringLen))
+			parser->current_key = KEY_INIT_INDEXES;
+		else if (!strncmp(key,"init_steps",stringLen))
+			parser->current_key = KEY_INIT_STEPS;
+		else parser->current_key = KEY_NONE;
+	} else if (parser->tree_state == TREE_STOP_ELEMENT) {
+		// We are in a stop element, expect a color component, "duration" or the "next_index" subkey
+		if (!strncmp(key,"red",stringLen))
+			parser->current_key = KEY_RED;
+		else if (!strncmp(key,"green",stringLen))
+			parser->current_key = KEY_GREEN;
+		else if (!strncmp(key,"blue",stringLen))
+			parser->current_key = KEY_BLUE;
+		else if (!strncmp(key,"white",stringLen))
+			parser->current_key = KEY_WHITE;
+		else if (!strncmp(key,"duration",stringLen))
+			parser->current_key = KEY_DURATION;
+		else if (!strncmp(key,"next_index",stringLen))
+			parser->current_key = KEY_NEXT_INDEX;
+		else parser->current_key = KEY_NONE;
+	} else parser->current_key = KEY_NONE;
+	return 1;
+}
+int animation::parser::yajl_start_map(void *ctx)
+{
+	animation::parser *parser = static_cast<animation::parser*>(ctx);
+	if (parser->skip_depth) {
+		parser->skip_depth++;
+		return 1;
+	}
+	switch (parser->current_key) {
+		case KEY_INIT_INDEXES:
+			parser->tree_state = TREE_INIT_INDEXES;
+			return 1;
+		case KEY_INIT_STEPS:
+			parser->tree_state = TREE_INIT_STEPS;
+			return 1;
+		case KEY_NONE:
+			if (parser->tree_state == TREE_TOP_LEVEL) 
+				parser->tree_state = TREE_ROOT_MAP;
+			else parser->skip_depth++; // Unknow key: skipping
+			return 1;
+		default:
+			if (parser->tree_state == TREE_STOPS_ARRAY) {
+				parser->tree_state = TREE_STOP_ELEMENT;
+				return 1;
+			} else {
+				parser->parse_error = "Unexpected map";
+				return 0;
+			}
+	}
+}
+int animation::parser::yajl_end_map(void *ctx)
+{
+	animation::parser *parser = static_cast<animation::parser*>(ctx);
+	if (parser->skip_depth) {
+		parser->skip_depth--;
+		return 1;
+	}
+	switch (parser->tree_state) {
+		case TREE_STOP_ELEMENT:
+			if (!parser->emit_stop(parser->current_stop))
+				return 0;
+			parser->reset_stop();
+			parser->tree_state = TREE_STOPS_ARRAY;
+			return 1;
+		case TREE_ROOT_MAP:
+			parser->tree_state = TREE_TOP_LEVEL;
+			return 1;
+		default:
+			parser->parse_error = "Unexpected end-of-map";
+			return 0;
+	}
+}
+int animation::parser::yajl_start_array(void *ctx)
+{
+	animation::parser *parser = static_cast<animation::parser*>(ctx);
+	if (parser->skip_depth) return 1;
+	switch (parser->current_key) {
+		case KEY_STOPS:
+			parser->tree_state = TREE_STOPS_ARRAY;
+			return 1;
+		case KEY_INIT_INDEXES:
+			parser->tree_state = TREE_INIT_INDEXES;
+			return 1;
+		case KEY_INIT_STEPS:
+			parser->tree_state = TREE_INIT_STEPS;
+			return 1;
+		default:
+			parser->parse_error = "Unexpected array";
+			return 0;
+	}
+}
+int animation::parser::yajl_end_array(void *ctx)
+{
+	animation::parser *parser = static_cast<animation::parser*>(ctx);
+	int ret_val;
+	if (!parser->skip_depth) {
+		parser->tree_state = TREE_ROOT_MAP;
+	}
+	return 1;
+}
---
+++ animation/parser.hpp
@@ -0,0 +1,128 @@
+#pragma once
+#include <yajl_parse.h>
+#include <animation.hpp>
+#include <string>
+namespace animation {
+	class parser {
+		private:
+			/** JSON parser handle
+			 *
+			 */
+			yajl_handle yajl_parser;
+			/** The current position in the tree
+			 *
+			 */
+			enum {
+				/** Root of the JSON
+				 */
+				TREE_TOP_LEVEL,
+				/** The top-level JSON map
+				 */
+				TREE_ROOT_MAP,
+				/** In the "stops" array
+				 *
+				 * But not in a stop element
+				 */
+				TREE_STOPS_ARRAY,
+				/** An element of the "stops" array
+				 */
+				TREE_STOP_ELEMENT,
+				/** In the "init_indexes" array
+				 */
+				TREE_INIT_INDEXES,
+				/** In the "init_step" array
+				 */
+				TREE_INIT_STEPS,
+			} tree_state = TREE_TOP_LEVEL;
+			/** Last key name
+			 *
+			 * This variable store the current key
+			 */
+			enum {
+				/** No or unknown key
+				 */
+				KEY_NONE,
+				/** "stops"
+				 */
+				KEY_STOPS,
+				/** "init_indexes"
+				 */
+				KEY_INIT_INDEXES,
+				/** "init_steps"
+				 */
+				KEY_INIT_STEPS,
+				/** "red"
+				 */
+				KEY_RED,
+				/** "green"
+				 */
+				KEY_GREEN,
+				/** "blue"
+				 */
+				KEY_BLUE,
+				/** "white"
+				 */
+				KEY_WHITE,
+				/** "duration"
+				 */
+				KEY_DURATION,
+				/** "next_index"
+				 */
+				KEY_NEXT_INDEX,
+			} current_key;
+			/** Depth in the skipped element
+			 *
+			 * This value is used to easily skip unknow keys.
+			 */
+			unsigned short skip_depth = 0;
+			static int yajl_null(void *ctx);
+			static int yajl_boolean(void *ctx, int boolVal);
+			static int yajl_integer(void *ctx, long long integerVal);
+			static int yajl_double (void *ctx, double doubleVal);
+			static int yajl_string (void *ctx, const unsigned char * stringVal, size_t stringLen);
+			
+			static int yajl_start_map(void *ctx);
+			static int yajl_map_key(void *ctx, const unsigned char * key, size_t stringLen);
+			static int yajl_end_map(void *ctx);
+			static int yajl_start_array(void *ctx);
+			static int yajl_end_array(void *ctx);
+			const yajl_callbacks callbacks = {yajl_null,yajl_boolean,yajl_integer,yajl_double,NULL,yajl_string,yajl_start_map,yajl_map_key,yajl_end_map,yajl_start_array,yajl_end_array};
+			
+			animation::stop<animation::basic_rgbw<double>> current_stop;
+			
+			/** Reset current_stop
+			 *
+			 * Clear current_stop member to defaults values (all zeros)
+			 */
+			void reset_stop(void);
+		protected:
+			/** Parse error string
+			 */
+			std::string parse_error;
+			
+			virtual int emit_initial_index(uint16_t value) = 0;
+			virtual int emit_initial_remaining(double value) = 0;
+			virtual int emit_stop(animation::stop<animation::basic_rgbw<double>> &stop) = 0;
+		public:
+			parser(void);
+			~parser(void);
+			/** Parse some datas
+			 */
+			yajl_status parse(const char *jsonText, size_t jsonTextLength) {
+				return yajl_parse(yajl_parser,reinterpret_cast<const unsigned char*>(jsonText),jsonTextLength);
+			}
+			/** End-parsing
+			 *
+			 * This function must be called because YAJL may still have unprocessed
+			 * datas in buffers because it can't determine the exact type.
+			 */
+			yajl_status complete_parse(void) {
+				return yajl_complete_parse(yajl_parser);
+			}
+			
+			/** Retrieve error string
+			 * \param status The result of the parse() or complete_parse() call
+			 */
+			std::string get_error(yajl_status status, const char *jsonText, size_t jsonTextLength);
+	};
+}
