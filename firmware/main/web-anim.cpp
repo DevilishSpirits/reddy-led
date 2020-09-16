1494f487e4f62e48eb1ea1769f5477db5b680d05
Luc Bournaud <bournaud@et.esiea.fr>
2020-09-16 11:03:28 +0200

    Fixed PUT /anim implementation
    	Now we return a success and output more logs

--- firmware/main/web-anim.cpp
+++ firmware/main/web-anim.cpp
@@ -24,21 +24,23 @@ static esp_err_t PUTanim(httpd_req_t *req)
 		if (status != yajl_status_ok) {
 			std::string error_message = parser.get_error(status,buffer,read);
 			httpd_resp_set_type(req,"text/plain");
-			httpd_resp_send_err(req,HTTPD_400_BAD_REQUEST,"Invalid request body");
+			httpd_resp_set_status(req,"400 Invalid JSON syntax");
 			return httpd_resp_send(req,error_message.c_str(),error_message.size());
 		}
 	} while (read);
 	// Check if YAJL liked the meat until the end
+	ESP_LOGD(LOG_TAG,"Animation syntax is correct.");
 	yajl_status status = parser.complete_parse();
 	if (status != yajl_status_ok) {
 		std::string error_message = parser.get_error(status,NULL,0);
 		httpd_resp_set_type(req,"text/plain");
-		httpd_resp_send_err(req,HTTPD_400_BAD_REQUEST,"Invalid request body");
-		return httpd_resp_send(req,error_message.c_str(),error_message.size());
+			httpd_resp_set_status(req,"400 Invalid JSON logic");
+			return httpd_resp_send(req,error_message.c_str(),error_message.size());
 	}
 	// Now return sucess
-	httpd_resp_set_status(req,"206 No Content");
-	return ESP_OK;
+	ESP_LOGI(LOG_TAG,"Animation upload successful");
+	const char sucess_reply[] = "Success!";
+	return httpd_resp_send(req,sucess_reply,sizeof(sucess_reply)-1);
 }
 
 const httpd_uri_t firmware::http_uri_PUTanim = {
